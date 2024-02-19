460298b18acd3469c4a23afefbed7a1456f0af3e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-19 05:32:48 +0300

    exercies first commit

--- index.js
+++ index.js
@@ -3,84 +3,28 @@ const app = express();
 
 app.use(express.json());
 
-let notes = [
+const persons = [
     {
         id: 1,
-        content: "HTML is easy",
-        important: true
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
         id: 2,
-        content: "Browser can execute only Javascript",
-        important: false
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
         id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
     }
 ]
-
-app.get('/', (request, response) => {
-    response.send('Learning Backend Development.')
-});
-
-app.get('/api/notes', (request, response) => {
-    return response.json(notes);
-});
-
-// generate Id
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    
-    return maxId + 1;
-}
-
-// POST request Receiving data
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: "Content missing"
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note);
-
-    response.json(note)
-});
-
-
-// Single source route
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => {
-        return note.id === id
-    });
-
-    if (note) {
-        return response.json(note);
-    } else {
-        return response.status(404).end()
-    }
-});
-
-// Resource removal route
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end();
-})
 
 const PORT = 3001;
 app.listen(PORT)
