e04639009637fe57896a319c7dd1f435154782af
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-13 05:00:23 +0300

    Error handler middleware

--- index.js
+++ index.js
@@ -10,23 +10,16 @@ app.use(express.json());
 app.use(cors());
 app.disable("x-powered-by");
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        response.status(400).send({ error: 'Malformatted Id' })
     }
-];
+
+    next(error);
+}
+
 
 app.get('/', (request, response) => {
     response.send('Learning Backend Development.')
@@ -60,7 +53,7 @@ app.post('/api/notes', (request, response) => {
 
 
 // Single source route
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
         if (note) {
          response.json(note)
@@ -68,19 +61,17 @@ app.get('/api/notes/:id', (request, response) => {
             response.status(404).end()
         }
     })
-    .catch(error => {
-        console.log(error);
-        response.status(400).send({ error: 'Malformatted Id' });
-    })
+    .catch(error => next(error))
 });
 
 // Resource removal route
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id)
-
     response.status(204).end();
 })
+
+// Error handler middleware
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT)
