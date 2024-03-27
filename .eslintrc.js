ca132079d9e03b2194d4cce28d3d0d864be49748
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-27 12:01:18 +0300

    eslint file configuration changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,34 @@
 module.exports = {
-    // ...
-    'plugins': [
-      '@stylistic/js'
+  // ...
+  'plugins': [
+    '@stylistic/js'
+  ],
+  'extends': 'eslint:recommended',
+  'rules': {
+    '@stylistic/js/indent': [
+      'error',
+      2
     ],
-    'extends': 'eslint:recommended',
-    'rules': {
-      '@stylistic/js/indent': [
-        'error',
-        2
-      ],
-      '@stylistic/js/linebreak-style': [
-        'error',
-        'unix'
-      ],
-      '@stylistic/js/quotes': [
-        'error',
-        'single'
-      ],
-      '@stylistic/js/semi': [
-        'error',
-        'never'
-      ],
-      'eqeqeq': 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': [
-        'error', 'always'
-      ],
-      'arrow-spacing': [
-        'error', { 'before': true, 'after': true }
-      ],
-      'no-console': 0
-    }
+    '@stylistic/js/linebreak-style': [
+      'error',
+      'unix'
+    ],
+    '@stylistic/js/quotes': [
+      'error',
+      'single'
+    ],
+    '@stylistic/js/semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0
   }
-  
+}
