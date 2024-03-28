3ded0881ceee110edf3d4737a75cf260cb05d39b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-28 05:44:36 +0300

    mongoose reference error resolved, imported mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config');
 const express = require("express");
+const mongoose = require('mongoose');
 const cors = require("cors");
 const app = express();
 const notesRouter = require('./controllers/notes');
