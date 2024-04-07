de716be68c98f86449fd6a322ebf6ab6fee72c35
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-07 09:28:40 +0300

    backend code cleanup

--- mongo.js
+++ mongo.js
@@ -13,29 +13,4 @@ const url =
 mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-});
-
-const Note = mongoose.model('Note', noteSchema);
-
-const note = new Note({
-    content: 'Mongoose makes things easy',
-    date: new Date(),
-    important: true
-});
-
-//note.save().then(result => {
-//    console.log('Note saved!');
-//    mongoose.connection.close();
-//})
-
-
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note);
-    })
-  mongoose.connection.close();
-})
+mongoose.connection.close();
