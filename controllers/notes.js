72378374c8dcb9ca3026fd45252400f8fb9067aa
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-22 09:11:49 +0300

    update: modified request to create new note with userId

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get("/notes", async (request, response) => {
     const notes = await Note.find({});
@@ -8,14 +9,20 @@ notesRouter.get("/notes", async (request, response) => {
 
 // POST request Receiving data
 notesRouter.post('/notes', async (request, response, next) => {
-    const body = request.body
+    const body = request.body;
+
+    const user = await User.findById(body.userId)
 
     const note = new Note({
         content: body.content,
-        important: body.important || false,
+        important: body.important === undefined ? false : body.important,
+        user: user.id
     });
 
     const savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+    
     response.status(201).json(savedNote);
    
 });
